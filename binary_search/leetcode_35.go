c92a26d01aa217eaaefa9a629906dadb4032316e
knbr13 <abdullahalaadine63@gmail.com>
2025-06-23 22:40:31 +0300

    solve leetcode problem nb 35

---
+++ binary_search/leetcode_35.go
@@ -0,0 +1,19 @@
+package binarysearch
+
+// searchInsert solves the problem described at: https://leetcode.com/problems/search-insert-position
+func searchInsert(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+
+	for l <= r {
+		m := l + (r-l)/2
+		if nums[m] == target {
+			return m
+		} else if nums[m] < target {
+			l = m + 1
+		} else {
+			r = m - 1
+		}
+	}
+
+	return l
+}
